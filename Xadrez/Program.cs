a8ccaa90ab02377258bba11cff157635197aa1a8
Fellipe Strombeck <fellipe.strombeck@gmail.com>
2022-10-11 17:40:58 -0300

    Criada classe Posicao

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using tabuleiro;
 
 namespace Xadrez
 {
@@ -10,6 +7,13 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+
+            Console.ReadLine();
         }
     }
 }
